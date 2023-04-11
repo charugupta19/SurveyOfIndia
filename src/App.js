329068c182274720153679606f2a4750e80241ce
charu.gupta <charu.gupta@ukg.com>
2023-04-11 23:01:51 +0530

    Update content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Content from './Content.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <>
+                <div className="main-panel">
+                    <div className="top-title">
+                        <div className="logoContent">
+                            <h2>Survey Of India</h2>
+                            <h2>Survey Of India</h2>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="content">
+                        <Content />
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
-
-export default App;
